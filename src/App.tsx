2151335bbde90b57169aea94f2235f45350a0f4d
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2023-01-26 21:58:45 -0300

    Basic implementation of ReactFlow

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import { useCallback, useState } from 'react';
+import ReactFlow, { Background, Connection, ConnectionMode, Controls, addEdge, useEdgesState, useNodesState } from 'reactflow';
+import * as Toolbar from '@radix-ui/react-toolbar'
+import 'reactflow/dist/style.css';
+import {zinc} from 'tailwindcss/colors'
+import { Square } from './components/nodes/Square';
+import { DefaultEdges } from './components/edges/DefaultEdge';
+
+
+const NODE_TYPES={
+  square: Square,
+}
+
+const EDGE_TYPES={
+  default: DefaultEdges,
+}
+
+const INITIAL_NODES = [
+  {
+   id: crypto.randomUUID(),
+   type: 'square',
+   position: {
+    x: 200,
+    y: 400,
+   },
+   data: {},
+  },
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+     x: 1000,
+     y: 400,
+    },
+    data: {},
+   },
+] satisfies Node[]
+
+function App() {
+
+  const [edges, setEdges, onEdgesChange] = useEdgesState([])
+  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES)
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges(edges => addEdge(connection, edges))
+  }, [])
+
+  function addSquareNode(){
+    setNodes(nodes => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position:{
+          x: 15,
+          y: 15,
+        },
+        data:{},
+      },
+    ])
+  } 
+  return (
+    <div className="w-screen h-screen">
+      <ReactFlow 
+        nodeTypes={NODE_TYPES}
+        edgeTypes={EDGE_TYPES}
+        nodes={nodes}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onNodesChange={onNodesChange}
+        connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{
+          type: 'default'
+        }}
+      >
+        <Background 
+          gap={12}
+          size={2}
+          color={zinc[200]}
+        />
+        <Controls/>
+      </ReactFlow>
+        <Toolbar.Root className='fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden'>
+          <Toolbar.Button 
+          onClick={() => addSquareNode()}
+          className='w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-4'/>
+        </Toolbar.Root>
+    </div>
+  )
+}
+
+export default App
